fb6dc8e775c7ef7e85ff608472f50c316b035c9d
m-grgic <70528482+m-grgic@users.noreply.github.com>
2026-02-04 09:57:08 +0100

    Update EnvelopePressing.svelte

--- src/lib/components/EnvelopePressing.svelte
+++ src/lib/components/EnvelopePressing.svelte
@@ -21,7 +21,7 @@
 
   const handlePointerUp = () => scale.set(1);
 
-  const drainRate = $derived(0.2 + pressValue / 120);
+  const drainRate = $derived(0.2 + pressValue / 180);
 
   const handlePointerDown = () => {
     scale.set(0.8);
